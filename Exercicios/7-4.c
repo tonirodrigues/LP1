9163f27c486552503e932bd124199d885d60124e
tonirodrigues <48607122+tonirodrigues@users.noreply.github.com>
2019-05-10 20:53:38 -0300

    Create 7-4.c

---
+++ Exercicios/7-4.c
@@ -0,0 +1,11 @@
+7-4) Explique com suas próprias palavras as diferentes abordagens dos exercícios 2 e 3.
+
+Qual abordagem você prefere usar e porquê?
+
+R: Utilização de ponteiros, acho mais fácil.
+
+
+Na sua opinião quais são as diferenças de uso de memória e velocidade nas duas abordagens?
+
+
+R: Na minha opnião a utilização diretamente com ponteiro, minimiza o uso de variaveis e com isso o uso de memória.
